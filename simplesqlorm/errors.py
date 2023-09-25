ed6758491d061b09dba00cefee2fb2bb7a73690c
Jonathan Voss <k98kurz@gmail.com>
2023-09-25 18:50:08 -0400

    added new error; fixed docblocks

--- simplesqlorm/errors.py
+++ simplesqlorm/errors.py
@@ -1,9 +1,18 @@
 def vert(condition: bool, error_message: str = '') -> None:
-    """If condition is true, raises a ValueError with the given message."""
+    """If condition is False, raises a ValueError with the given message."""
     if not condition:
         raise ValueError(error_message)
 
 def tert(condition: bool, error_message: str = '') -> None:
-    """If condition is true, raises a TypeError with the given message."""
+    """If condition is False, raises a TypeError with the given message."""
     if not condition:
         raise TypeError(error_message)
+
+
+class UsageError(BaseException):
+    ...
+
+def tressa(condition: bool, error_message: str = '') -> None:
+    """If condition is False, raises a UsageError with the given message."""
+    if not condition:
+        raise UsageError(error_message)
